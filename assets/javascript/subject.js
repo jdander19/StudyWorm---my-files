8e2c09696bc17ed7ed2d6dd48fbc512be1c00ce2
William Stephan <william.stephan@icloud.com>
2020-01-28 21:08:01 -0600

    initial user and subject objects

---
+++ assets/javascript/subject.js
@@ -0,0 +1,32 @@
+console.log("subject.js connected");
+
+let subjectArr = new Array(0); // subject object array
+let subList = new Array(0); // subject list
+
+class Subject {
+    constructor(subjectName, goal) {
+        this.subject = subjectName;
+        this.goal = goal; // float - hrs 
+        this.hours; // float - hours worked (total timer)
+        this.complete = false;
+        subjectArr.push(this) // stores objects in array
+        subList.push(this.subject); // stores subject name in array 
+    }
+}
+
+function initSubject(subjectName, goal) {
+    if (!subList.includes(subjectName)) { // checks if subject exists
+        new Subject(subjectName, goal)
+    } else {
+        console.log("subject already exists"); // reject notice
+        alert("need subject exists notificaion!");
+    }
+
+}
+
+initSubject("Math", 50);
+initSubject("Science", 50);
+
+for (var i = 0; i < subjectArr.length; i++) {
+    console.log(subjectArr[i]);
+} // reset gem objects
--- assets/javascript/user.js
+++ assets/javascript/user.js
@@ -1,7 +1,29 @@
-var email = "billstephan@yahoo.com";
-var password = "MY!Password2";
+console.log("user.js connected");
 
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-  console.log(error.code);
-  console.log(error.message);
-});
+class User {
+  constructor(email) {
+    this.email = email;
+    this.name // need
+    this.password // need
+    this.city;
+    this.state;
+    this.edLevel; // need
+    this.ipAddress;
+    this.create; // need timestamp
+    this.lastActive; // need timestamp
+    this.location;
+  }
+}
+
+function initUser(email) {
+  user = new User(email);
+  user.ipAddress = geoplugin_request();
+  user.city = geoplugin_city();
+  user.state = geoplugin_regionName();
+  user.location = [geoplugin_latitude(),
+    geoplugin_longitude()
+  ]
+}
+
+initUser('me@billstephan.com');
+console.log(user);
